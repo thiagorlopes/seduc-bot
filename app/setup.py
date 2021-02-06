ca6fa7b724d2a5b1eb661fa3d0113eec68fdd361
thiago.rlopes <thiago.rlopes@outlook.com>
2021-02-06 09:09:33 -0300

    Change code styling.
    * Include Procfile on .gitignore;
    * Move sendMessage to outside of if clause for preventing code
    repetition;
    * Change code styling for PEP8 compliance.

--- app/setup.py
+++ app/setup.py
@@ -4,6 +4,7 @@ import telegram
 from telebot.credentials import bot_token, bot_user_name, URL
 import codigo
 
+
 global bot
 global TOKEN 
 TOKEN = bot_token
@@ -11,41 +12,39 @@ bot = telegram.Bot(token=TOKEN)
 
 app = Flask(__name__)
 
+
 @app.route('/{}'.format(TOKEN), methods=['POST'])
 def respond():
 
     # Obtém mensagem em JSON e a transforma em um objeto Telegram
-
     update = telegram.Update.de_json(request.get_json(force=True), bot)
 
     chat_id = update.message.chat_id
     msg_id = update.message.message_id
     text = update.message.text.encode('utf-8').decode()
+    bot_reply = ""
 
     if text == "/start":
-
-        bot_welcome = """
-
-        Bem vindo ao assistente de composições analíticas da SEDUC, digite o código desejado. Tabelas disponíveis: Sinapi DEZ/2020 e Composições SEDUC"""
-
-        bot.sendMessage(chat_id=chat_id, text=bot_welcome, reply_to_message_id=msg_id)
-
+        bot_reply = """Bem vindo ao assistente de composições analíticas da
+        SEDUC, digite o código desejado. Tabelas disponíveis: Sinapi DEZ/2020
+        e Composições SEDUC"""
     else:
         try:
             text = text.upper()
 
             if text in codigo.SINAPI:
-                answer = codigo.SINAPI[text]          
+                bot_reply = codigo.SINAPI[text]
             else:
-                answer = "Código não encontrado"
-            bot.sendMessage(chat_id=chat_id, text=answer, reply_to_message_id=msg_id)
-            
+                bot_reply = "Código não encontrado"
 
         except Exception:
+            bot_reply = """Código não encontrado nas tabelas de referência
+            SINAPI DEZ/20 e COMP SEDUC, tente novamente. Ex: Sinapi 93281, COMP 001"""
 
-            bot.sendMessage(chat_id=chat_id, text="""Código não encontrado nas tabelas de referência SINAPI DEZ/20 e COMP SEDUC, tente novamente. Ex: Sinapi 93281, COMP 001""", reply_to_message_id=msg_id)
+    bot.sendMessage(chat_id=chat_id, text=bot_reply, reply_to_message_id=msg_id)
 
     return "ok"
+
 
 @app.route('/set_webhook', methods=['GET', 'POST'])
 def set_webhook():
@@ -54,6 +53,7 @@ def set_webhook():
         return "Webhook setup was successful"
     else:
         return "Webhook setup failed"
+
 
 @app.route('/')
 def index():
